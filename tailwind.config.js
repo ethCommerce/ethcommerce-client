ce0ca681be21d8cd22fd87b67b26f00061abfdae
Sibbe Heijne <sibbe.heijne@kpn.com>
2020-10-27 23:21:43 +0100

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      flex: {
+        fill: '1 0 100%',
+        static: '0 0 auto',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
