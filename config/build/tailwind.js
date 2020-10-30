7df01d5fbd7c8848481b11e405c07e8cf5a1ac58
Sibbe Heijne <sibbe.heijne@kpn.com>
2020-10-30 11:00:24 +0100

    feat(build): Extend tailwindcss config

--- config/build/tailwind.js
+++ config/build/tailwind.js
@@ -1,12 +1,54 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.svelte',
+  ],
+  corePlugins: [
+    'preflight',
+    'alignItems',
+    'backgroundColor',
+    'backgroundOpacity',
+		'display',
+    'flex',
+    'flexDirection',
+    'gridTemplateColumns',
+    'gridTemplateRows',
+    'inset',
+    'justifyContent',
+    'height',
+    'margin',
+		'maxHeight',
+    'maxWidth',
+    'minHeight',
+    'minWidth',
+    'order',
+    'overflow',
+    'padding',
+    'position',
+    'textColor',
+    'width',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.gray,
+        secondary: colors.blue,
+      },
+      sidebar: {
+        size: '4rem',
+      },
+      flex: {
+        fill: '1 1 100%',
+      },
+      inset: {
+        '100': '100%',
+      },
+    },
   },
-  variants: {},
-  plugins: [],
-}
+};
